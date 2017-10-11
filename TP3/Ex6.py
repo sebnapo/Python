a6badfda5db87ca69e0f90fae54e828ddd625b45
unknown <wejden.elareron@gmail.com>
2017-10-11 10:06:51 +0200

    jegagner

--- TP3/Ex6.py
+++ TP3/Ex6.py
@@ -1,80 +1,49 @@
 from tkinter import *
-def hanoi(n,i,j,tab):
+def hanoi(n):
+    etat = [[], [], []]
 
+    for i in range(n):
+        etat[0].append(i)
+
+    hanoiVrai(n,1,n,etat)
+
+def hanoiVrai(n,i,j,etat):
     if (n > 0):
-        hanoi(n-1,i,6-i-j,tab)
+        printHanoi(etat)
+        hanoiVrai(n-1,i,6-i-j,etat)
         print ("Déplace " + str(i) +"sur" + str(j))
+        etat = changement(etat, i-1, j-1)
+        printHanoi(etat)
 
-        tab.append([i,j])
-
-
-        hanoi(n-1,6-i-j,j,tab)
-
-
-
-def etatHanoi(tab, n):
-
-    etats = []
-
-    placement = [n , 0, 0]
-
-    etats.append(placement)
-
-    for i in range (len(tab) - 1):
-
-        newPlacement = [0, 0, 0]
-
-        newPlacement[tab[i][0] - 1] = placement[tab[i][0] - 1] - 1
-        newPlacement[tab[i][1] - 1] = placement[tab[i][1] - 1] + 1
-
-
-
-        etats.append(newPlacement)
-
-    return etats
-
-tab = []
-
-hanoi(3,1,3,tab)
-
-print(tab)
-
-print(etatHanoi(tab, 3))
+        hanoiVrai(n-1,6-i-j,j,etat)
 
 
 
 
+def changement(etat,a,b):
+
+    move = etat[a][len(etat[a])-1]
+    etat[a].pop()
+    etat[b].append(move)
+
+    return(etat)
+
+def printHanoi(etat):
+
+    print("||||||||||||||||||||||||")
+
+    for i in range(3):
+
+        pile = "["
+
+        for j in range(len(etat[i])):
+
+            pile += str(etat[i][j]+1)
+
+        print(pile)
 
 
 
 
-'''
-
-def jeuHanoi():
-
-    tabDeplacements = []
-
-    hanoi(3, 1, 3, tabDeplacements)
-
-    print(tabDeplacements)
-
-
-
-
-    n = len(tabDeplacements) - 1
-    i = 0
-
-    suivant = True
-    while(suivant and (i <= n)):
-
-        suivant = False
-        i += 1
-
-'''
-
-
-
-
-
-
+hanoi(3)
 
