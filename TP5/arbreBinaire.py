38a608541b834059313cb3ac6bda0ab189c7bdb3
unknown <wejden.elareron@gmail.com>
2017-11-06 14:51:15 +0100

    oui

--- TP5/arbreBinaire.py
+++ TP5/arbreBinaire.py
@@ -1,3 +1,5 @@
+import numpy
+
 class arbre:
     def __init__(self,racine=None,nbElem=1):
         self.racine = racine
@@ -23,7 +25,7 @@ def term():
 
 
 g = arbre(noeud(1,noeud(0,term(),term()),noeud(5,term(),term())))
-d = arbre(noeud(4,noeud(3,term(),term()),noeud(5,term(),term())))
+d = arbre(noeud(4,noeud(3,term(),term()),noeud(5,noeud(5,term(),term()),term())))
 a = arbre(noeud(2,g.root(),d.root()))
 
 def afficherArbre(noeud):
@@ -39,6 +41,17 @@ afficherArbre(a.racine)
 
 def depth(arbre):
     paths = []
-    pathfind(arbre.racine)
+    pathfind(arbre.racine, paths)
+    print(paths)
+    return max(paths)
 
-def pathfind(noeud):
+
+def pathfind(noeud, paths, count=0):
+    if(noeud == None):
+        paths.append(count)
+    else:
+        count += 1
+        pathfind(noeud.gauche, paths, count)
+        pathfind(noeud.droite, paths, count)
+
+print(depth(a))
