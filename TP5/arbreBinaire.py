db394692359dea92c8fbe7ae720b0a73eb4529d8
unknown <wejden.elareron@gmail.com>
2017-11-06 14:41:50 +0100

    pd

---
+++ TP5/arbreBinaire.py
@@ -0,0 +1,44 @@
+class arbre:
+    def __init__(self,racine=None,nbElem=1):
+        self.racine = racine
+        self.nbElem = nbElem
+
+    def empty(self):
+        if(self.racine == None):
+            return True
+        else: return False
+
+    def root(self):
+        return self.racine
+
+
+class noeud:
+    def __init__(self,val, droite, gauche):
+        self.val = val
+        self.droite = droite
+        self.gauche = gauche
+
+def term():
+    return None
+
+
+g = arbre(noeud(1,noeud(0,term(),term()),noeud(5,term(),term())))
+d = arbre(noeud(4,noeud(3,term(),term()),noeud(5,term(),term())))
+a = arbre(noeud(2,g.root(),d.root()))
+
+def afficherArbre(noeud):
+    if(noeud == None):
+        return 0
+    else:
+        print(noeud.val)
+        afficherArbre(noeud.droite)
+        afficherArbre(noeud.gauche)
+
+
+afficherArbre(a.racine)
+
+def depth(arbre):
+    paths = []
+    pathfind(arbre.racine)
+
+def pathfind(noeud):
