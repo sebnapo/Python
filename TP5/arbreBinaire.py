30872fd63f072c5e26381f701f3edeab98ed95b0
= <naposebastien@gmail.com>
2017-11-06 19:22:03 +0100

    LOL JE TAFF CHEZ MOI KHEY

--- TP5/arbreBinaire.py
+++ TP5/arbreBinaire.py
@@ -22,9 +22,9 @@ def term():
     return None
 
 
-g = arbre(noeud(1,noeud(0,term(),term()),noeud(5,term(),term())))
-d = arbre(noeud(4,noeud(3,term(),term()),noeud(8,term(),term())))
-a = arbre(noeud(2,g.root(),d.root()))
+g = arbre(noeud(11,noeud(12,term(),term()),noeud(7,term(),term())))
+d = arbre(noeud(2,noeud(4,term(),term()),noeud(1,term(),term())))
+a = arbre(noeud(6,g.root(),d.root()))
 
 def afficherArbre(noeud):
     if(noeud == None):
@@ -33,9 +33,6 @@ def afficherArbre(noeud):
         print(noeud.val)
         afficherArbre(noeud.droite)
         afficherArbre(noeud.gauche)
-
-
-afficherArbre(a.racine)
 
 def depth(arbre):
     paths = []
@@ -58,7 +55,7 @@ def pathfind(noeud, paths, count=0):
 def nb_node(arbre):
     nodes =  []
     pathfind(arbre.racine, nodes)
-    return len(nodes)
+    return len(nodes)-1
 
 def summ(arbre):
     somme = []
@@ -87,20 +84,93 @@ def incRecursif(noeud):
         incRecursif(noeud.gauche)
 
 def hierarchie(arbre):
-    hierarchieRec(arbre.racine)
+    bool = hierarchieRec(arbre.racine)
+    return bool
 
 
 def hierarchieRec(noeud,boolean=True):
     if(noeud == None):
+        print(boolean)
         return boolean
     else:
-        if(noeud.val < noeud.droite.val )
+        if(noeud.droite != None):
+            if(noeud.val < noeud.droite.val ):
+                boolean = False
+                return boolean
+        if(noeud.gauche != None):
+            if(noeud.val < noeud.gauche.val):
+                boolean = False
+                return boolean
+        hierarchieRec(noeud.droite,boolean)
+        hierarchieRec(noeud.gauche,boolean)
 
-
+print("Affichage de l'arbre a :")
+afficherArbre(a.racine)
 print("Nombre node :")
 print(nb_node(a))
 
 print("somme " + str(summ(a)))
 
+print("Incrémentation de 1 de chaque noeud de l'arbre : ")
+#afficherArbre(inc(a).racine)
+print("Les fils sont-il tout inférieur aux parents ?")
+print(hierarchie(a))
+
+##------------------Partie arbre binaire de recherche-------------------##
+def InsertionAbr(arbre,val):
+    InsertionAbrRecursif(arbre.racine,val)
+
+def InsertionAbrRecursif(node,val):
+    if(node == None):
+        return 0
+    if(node.gauche == None and node.val >= val):
+        node.gauche = noeud(val,term(),term())
+    elif(node.gauche != None and node.val >= val):
+        InsertionAbrRecursif(node.gauche,val)
+    elif(node.droite == None):
+        node.droite = noeud(val,term(),term())
+    else:
+        InsertionAbrRecursif(node.droite,val)
+
+
+def InsertionAbrIteratif(arbre,val):
+    inserer = False
+    node = arbre.racine
+    while(inserer == False):
+        if(val <= node.val and node.gauche == None):
+            node.gauche = noeud(val,term(),term())
+            inserer = True
+        elif(val <= node.val):
+            node = node.gauche
+        elif(node.droite == None):
+            node.droite = noeud(val,term(),term())
+            inserer = True
+        elif(val > node.val):
+            node = node.droite
+
+def RechercheAbr(arbre,val):
+    res = RechercheAbrRecursif(arbre.racine,val)
+    print(res)
+    return res
+
+def RechercheAbrRecursif(node,val,find=False):
+    if (node == None):
+        print("1")
+        return find
+    if(node.val == val):
+        print("2")
+        find = True
+    if (node.val >= val):
+        print("3")
+        RechercheAbrRecursif(node.gauche,val,find)
+    elif (node.val <= val):
+        print("4")
+        RechercheAbrRecursif(node.droite,val,find)
+    print(find)
+
+
+InsertionAbr(a,1)
+InsertionAbrIteratif(a,0)
 afficherArbre(a.racine)
-afficherArbre(inc(a).racine)
+print("recherche recursive :")
+print(RechercheAbr(a,12))
